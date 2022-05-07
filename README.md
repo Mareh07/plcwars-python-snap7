7cb0c1c761a03a596234a92de74757624ecda7e1
Mareh07 <marekhalaczek@gazeta.pl>
2022-05-07 16:33:44 +0200

    added functions for reading marker values, readme update

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 Python Snap7 (PUT/GET) communication with Siemens PLC TIA Portal
 
-* Link for the yt tutorial:
+* Link for the yt tutorial (basics): https://youtu.be/xSTfmCdepyw
 * Script based on: https://github.com/gijzelaerr/python-snap7/blob/master/example/boolean.py
 * Snap7 homepage: http://snap7.sourceforge.net/
 * PUT/GET S7 Communication: https://cache.industry.siemens.com/dl/files/115/82212115/att_108330/v2/82212115_s7_communication_s7-1500_en.pdf
+
+* Link for the yt tutorial (basics):
+* Snap7 documentation: https://buildmedia.readthedocs.org/media/pdf/python-snap7/latest/python-snap7.pdf
+* Convert Byte array: https://stackoverflow.com/questions/56396524/how-to-convert-a-byte-array-to-float-in-python
+* Elementary data types: http://www.plcdev.com/step_7_elementary_data_types
--- read_write_snap7.py
+++ read_write_snap7.py
@@ -1,39 +1,58 @@
 """
-Based on:
-https://github.com/gijzelaerr/python-snap7/blob/master/example/boolean.py
-the minimum amount of data being read or written to a plc is 1 byte...
+Functions writeBool and readBool are based on:
+	https://github.com/gijzelaerr/python-snap7/blob/master/example/boolean.py
+	the minimum amount of data being read or written to a plc is 1 byte
 
-Constraints:
-get/put get has to be enabled
-virtual adapter from plc sim advanced has to be used
-DB cannot be optimized
+Functions readMemory and writeMemory are based on:
+	https://buildmedia.readthedocs.org/media/pdf/python-snap7/latest/python-snap7.pdf
+	Python snap7 documentation
+
+General Constraints:
+	get/put get has to be enabled
+	virtual adapter from plc sim advanced has to be used
+	DB cannot be optimized
 
 """
 
 import snap7
+import struct
 
 plc = snap7.client.Client()
-plc.connect('192.168.0.1', 0, 1)  # IP address, rack, slot (from HW settings)
+plc.connect('192.168.0.2', 0, 1)  # IP address, rack, slot (from HW settings)
 
 db_number = 2
 start_offset = 0
 bit_offset = 0
 value = 1  # 1 = true | 0 = false
 
+start_address = 100  # starting address
+length = 4  # double word
 
-def WriteBool(db_number, start_offset, bit_offset, value):
+def writeBool(db_number, start_offset, bit_offset, value):
 	reading = plc.db_read(db_number, start_offset, 1)    # (db number, start offset, read 1 byte)
-	snap7.util.set_bool(reading, 0, bit_offset, value)   # (x,x, bit number, value 1= true;0=false) (bytearray_: bytearray, byte_index: int, bool_index: int, value: bool)
+	snap7.util.set_bool(reading, 0, bit_offset, value)   # (value 1= true;0=false) (bytearray_: bytearray, byte_index: int, bool_index: int, value: bool)
 	plc.db_write(db_number, start_offset, reading)       #  write back the bytearray and now the boolean value is changed in the PLC.
 	return None
 
-def ReadBool(db_number, start_offset, bit_offset):
+def readBool(db_number, start_offset, bit_offset):
 	reading = plc.db_read(db_number, start_offset, 1)  
 	a = snap7.util.get_bool(reading, 0, bit_offset)
 	print('DB Number: ' + str(db_number) + ' Bit: ' + str(start_offset) + '.' + str(bit_offset) + ' Value: ' + str(a))
+	return None
+
+def readMemory(start_address,length):
+	reading = plc.read_area(snap7.types.Areas.MK, 0, start_address, length)
+	value = struct.unpack('>f', reading)  # big-endian
+	print('Start Address: ' + str(start_address) + ' Value: ' + str(value))
+
+def writeMemory(start_address,length,value):
+	plc.mb_write(start_address, length, bytearray(struct.pack('>f', value)))  # big-endian
+	print('Start Address: ' + str(start_address) + ' Value: ' + str(value))
+
+# writeBool(db_number, start_offset, bit_offset, value)
+#
+# readBool(db_number, start_offset, bit_offset)
 
 
-WriteBool(db_number, start_offset, bit_offset, value)
-
-ReadBool(db_number, start_offset, bit_offset)
-
+writeMemory(start_address, length, 786.78)
+# readMemory(start_address, length)
